9f8e6b7c7eeed832e8c806d9ceacb9850f6c2573
Madiiboy <33929650+Madiiboy@users.noreply.github.com>
2019-01-05 20:17:17 +0100

    upload activity

---
+++ app/src/main/java/com/example/jdeiv/picoftheday/PicofthedayFragment.kt
@@ -0,0 +1,17 @@
+package com.example.jdeiv.picoftheday
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PicofthedayFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.fragment_home, container, false)
+
+    companion object {
+        fun newInstance(): PicofthedayFragment = PicofthedayFragment()
+    }
+
+}
