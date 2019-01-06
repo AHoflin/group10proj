994b91622795505a6cdad9a4effec850b162e63e
Madiiboy <33929650+Madiiboy@users.noreply.github.com>
2019-01-06 15:27:43 +0100

    Position and upload activity
    No design implemented. Looks like shit

---
+++ app/src/main/java/com/example/jdeiv/picoftheday/LocationTask.kt
@@ -0,0 +1,82 @@
+package com.example.jdeiv.picoftheday
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.AsyncTask
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import java.io.File
+
+
+class LocationTask(private var context: Context, private var fusedLocationProviderClient: FusedLocationProviderClient): AsyncTask<Any, Any, Any>() {
+    private var lng : Double? = null
+    private var lat : Double? = null
+    lateinit var locationManager: LocationManager
+
+    override fun onPreExecute() {
+        super.onPreExecute()
+        if(ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+                //On success it can be null. Then it goes bananas
+                Log.d("hejhej", "1")
+                if(it != null) {
+                    lng = it.longitude
+                    lat = it.latitude
+                    //Toast.makeText(context, lng.toString(), Toast.LENGTH_SHORT).show()
+                    Log.d("LocationTask", "Pos found")
+
+                }else{
+                    locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0F, object : LocationListener{
+                        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                        }
+
+                        override fun onProviderEnabled(provider: String?) {
+                            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                        }
+
+                        override fun onProviderDisabled(provider: String?) {
+                            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                        }
+
+                        override fun onLocationChanged(location: Location?) {
+
+                            if(location != null){
+                                lng = location!!.longitude
+                                lat = location!!.latitude
+                                Log.d("LocationTask", location.longitude.toString())
+                                Log.d("LocationTask", location.latitude.toString())
+                            }else{
+                                Toast.makeText(context, "Could not find position", Toast.LENGTH_SHORT).show()
+                                //kill application here or something
+                            }
+                        }
+                    })
+                }
+            }
+        }
+    }
+
+    override fun doInBackground(vararg params: Any?): Any {
+        while(lng == null && lat == null){}
+        return ""
+    }
+
+    override fun onPostExecute(result: Any?) {
+        super.onPostExecute(result)
+
+        val fileName = "/location.txt"
+        val file = File(context.dataDir.toString() + fileName)
+        file.bufferedWriter().use { out -> out.write(lng.toString()+ "\n" + lat.toString()) }
+    }
+}
+
+
+
--- app/src/main/java/com/example/jdeiv/picoftheday/MainActivity.kt
+++ app/src/main/java/com/example/jdeiv/picoftheday/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.example.jdeiv.picoftheday
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.ActionBar
+import android.support.v7.app.AlertDialog
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationServices
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), com.google.android.gms.location.LocationListener {
 
     lateinit var toolbar: ActionBar
+    private var REQUEST_LOCATION_CODE = 101
+    private var mGoogleApiClient: GoogleApiClient? = null
 
     private fun openFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
@@ -24,6 +39,8 @@ class MainActivity : AppCompatActivity() {
                 toolbar.title = "Camera"
                 val cameraFragment = CameraFragment.newInstance()
                 openFragment(cameraFragment)
+                val intent = Intent(this, UploadActivity::class.java)
+                startActivity(intent)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_home -> {
@@ -47,11 +64,109 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        buildGoogleApiClient()
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //Location Permission already granted
+            checkLocationPermission()
+        }
+
+        if (!checkGPSEnabled()) {
+        }else {
+            Log.d("hejhej", "1")
+            LocationTask(this, LocationServices.getFusedLocationProviderClient(this)).execute()
+        }
+
         toolbar = supportActionBar!!
         val bottomNavigation: BottomNavigationView = findViewById(R.id.bottomNavigationView)
         bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
     }
 
+    override fun onLocationChanged(location: Location?) {
+        //Boilerplate mumbojumbo
+    }
+
+    @Synchronized
+    private fun buildGoogleApiClient() {
+
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+            .addApi(LocationServices.API)
+            .build()
+        mGoogleApiClient!!.connect()
+
+    }
+
+    private fun checkGPSEnabled(): Boolean {
+        if (!isLocationEnabled())
+            showAlert()
+        return isLocationEnabled()
+    }
+
+    private fun showAlert() {
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle("Enable Location")
+            .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " + "use this app")
+            .setPositiveButton("Location Settings") { paramDialogInterface, paramInt ->
+                val myIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(myIntent)
+            }
+            .setNegativeButton("Cancel") { paramDialogInterface, paramInt -> }
+        dialog.show()
+
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager!!.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager!!.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun checkLocationPermission() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                AlertDialog.Builder(this)
+                    .setTitle("Location Permission Needed")
+                    .setMessage("This app needs the Location permission, please accept to use location functionality")
+                    .setPositiveButton("OK", { dialog, which ->
+                        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_CODE)
+                    })
+                    .create()
+                    .show()
+
+            } else ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_LOCATION_CODE -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "permission granted", Toast.LENGTH_LONG).show()
+                    }
+                } else {
+                    // permission denied, boo! Disable the functionality that depends on this permission.
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show()
+                }
+                return
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mGoogleApiClient?.connect()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (mGoogleApiClient!!.isConnected()) {
+            mGoogleApiClient!!.disconnect()
+        }
+    }
 
 }
