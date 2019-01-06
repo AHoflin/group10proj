640a7ff724b7b6977c49d3bbc9e57d90152f6911
Adam <adamhoflin98@gmail.com>
2019-01-06 21:19:06 +0100

    Added back button for settings page.

--- app/src/main/java/com/example/jdeiv/picoftheday/SettingsActivity.kt
+++ app/src/main/java/com/example/jdeiv/picoftheday/SettingsActivity.kt
@@ -3,6 +3,9 @@ package com.example.jdeiv.picoftheday
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_settings.*
 
@@ -16,6 +19,27 @@ class SettingsActivity : AppCompatActivity() {
             FirebaseAuth.getInstance().signOut()
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+        }
+
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflating the toolbar menu
+        menuInflater.inflate(R.menu.top_menu_settings, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.getItemId()
+
+        if (id == R.id.back_arrow_settings){
+            // Send user back to MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            /* The following return should not run, but is necessary in order to compile */
+            return false
+        } else {
+            return super.onOptionsItemSelected(item)
         }
     }
 }
