d6f1c9345fad4be9716a01352bfe9c20da359eea
Mats Fridberg <mfg17006@student.mdh.se>
2019-01-17 17:21:45 +0100

    Tiny push. No krock

--- app/src/main/java/com/example/jdeiv/picoftheday/CameraFragment.kt
+++ app/src/main/java/com/example/jdeiv/picoftheday/CameraFragment.kt
@@ -34,6 +34,7 @@ import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.fragment_camera.view.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.activity_settings.*
 
 
 class CameraFragment : Fragment() {
@@ -53,6 +54,8 @@ class CameraFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_camera, container, false)
         setHasOptionsMenu(true)
+
+
 
         if (savedInstanceState != null){
             Log.d("CameraFragment", "savedInstanceState != null")
