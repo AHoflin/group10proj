30b05d8f4ffa723219f8a045fdb8757ceccaec0d
Adam <adamhoflin98@gmail.com>
2019-01-18 01:57:57 +0100

    push

--- app/src/main/java/com/example/jdeiv/picoftheday/LocationTask.kt
+++ app/src/main/java/com/example/jdeiv/picoftheday/LocationTask.kt
@@ -32,7 +32,7 @@ class LocationTask(private var context: Context, private var fusedLocationProvid
         // Workaround buggfix
         val fileName = "/location.txt"
         val file = File(context.dataDir.toString() + fileName)
-        file.bufferedWriter().use { out -> out.write("13.37"+ "\n" + "4.20") }
+        file.bufferedWriter().use { out -> out.write("59.618631"+ "\n" + "16.540509") }
 
         if(ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             fusedLocationProviderClient.lastLocation.addOnSuccessListener {
